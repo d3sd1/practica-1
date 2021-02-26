c35f104325bcd4bccfbaa2c8035efe133740a9ff
andreigarcia <andreigarciacuadra@gmail.com>
2021-02-26 05:17:19 +0100

    upgrade

--- myls.c
+++ myls.c
@@ -7,5 +7,21 @@
 
 int main(int argc, char *argv[]) {
 
+    DIR *descriptor;
+    if (argc < 2) {
+        char buf[PATH_MAX];
+        getcwd(buf, PATH_MAX);
+        descriptor = opendir(buf);
+    }
+    if (descriptor == NULL) {
+        printf("Error opening file.\n");
+        return -1;
+    }
+
+    struct dirent *file;
+    while ((file = readdir(descriptor)) != NULL) {
+        printf("%s\n", file->d_name);
+    }
+
     return 0;
 }
--- mysize.c
+++ mysize.c
@@ -8,5 +8,32 @@
 
 int main(int argc, char *argv[]) {
 
+    DIR *descriptor;
+    char buffer[PATH_MAX];
+    getcwd(buffer, PATH_MAX);
+    descriptor = opendir(buffer);
+
+    struct dirent *file;
+    mainReader:
+    while ((file = readdir(descriptor)) != NULL) {
+            int fDescriptor = open(file->d_name, O_RDONLY);
+            if (fDescriptor < 0) {
+                printf("Error opening file -> %s\n", file->d_name);
+                goto mainReader;
+            }
+            int fileSize = lseek(fDescriptor, 0, SEEK_END);
+            if (fileSize < 0) {
+                printf("Error closing file -> %s\n", file->d_name);
+                goto mainReader;
+            }
+            printf("%s    %d\n", file->d_name, fileSize);
+
+            if (close(fDescriptor) < 0) {
+                printf("Error closing file -> %s\n", file->d_name);
+                return -1;
+            }
+
+
+    }
     return 0;
 }
