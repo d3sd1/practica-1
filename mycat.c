fd1825f1524c59028cbee7e3661ee7bcb249fbf9
andreigarcia <andreigarciacuadra@gmail.com>
2021-02-28 06:12:19 +0100

    mycat upgrade

--- mycat.c
+++ mycat.c
@@ -16,12 +16,19 @@ int main(int argc, char *argv[]) {
     int descriptor;    //Descriptor del fichero
     char buf[BUFFER_SIZE];    //Declaramos el buffer con el tamaño indicado
 
+
+    /*
+     * Read file and fetch descriptor.
+     */
     descriptor = open(argv[1], O_RDONLY);
     if (descriptor < 0) {
         printf("Error opening file.\n");
         return -1;
     }
 
+    /*
+     * Read file line to line and output with STDOUT_FILENO
+     */
     int nread, nwrite;
     while ((nread = read(descriptor, buf, BUFFER_SIZE)) > 0) {
         do {
@@ -38,6 +45,24 @@ int main(int argc, char *argv[]) {
 
         } while (nread > 0);
     }
+    /*
+     * We faced an error.
+     */
+    if (nread < 0) {
+        printf("Error reading file.\n");
+        if (close(descriptor) < 0) {
+            printf("Error while closing file.\n");
+        }
+        return -1;
 
+    }
+
+    /*
+     * Free memory, close descriptor.
+     */
+    if (close(descriptor) < 0) {
+        printf("Error closing file.\n");
+        return -1;
+    }
     return 0;
 }
